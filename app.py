9c0ff5910c6a2ded3940f2786d6653a3499056cd
sooraj <soorajks2002@gmail.com>
2023-08-19 19:43:53 +0530

    streamlit app

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from kmeans_segmentation import segment_image
+
+img = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
+number_of_segments = st.number_input(
+    'Select number of segment', min_value=1, max_value=25,)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Original Image")
+    st.image(img)
+
+if img and number_of_segments > 0:
+    img = Image.open(img)
+    img = np.asarray(img)
+    with col2:
+        st.header("Segmented Image")
+        st.image(segment_image(img, number_of_segments))
