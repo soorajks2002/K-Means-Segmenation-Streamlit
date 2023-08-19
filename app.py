b2299e44926e5a6e47a63517ad336a13367ae978
sooraj <soorajks2002@gmail.com>
2023-08-19 22:00:12 +0530

    Updated UI

--- app.py
+++ app.py
@@ -3,15 +3,21 @@ from PIL import Image
 import numpy as np
 from kmeans_segmentation import segment_image
 
-img = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
+st.set_page_config(page_title="KMeans Segmentation", layout="wide")
+
+st.title("Image Segmentation Using K-Means Clustering")
+
+img = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'], label_visibility='visible')
+
 number_of_segments = st.number_input(
-    'Select number of segment', min_value=1, max_value=25,)
+    'Select number of segment', min_value=1, max_value=25, value=2, label_visibility='visible')
 
 col1, col2 = st.columns(2)
 
-with col1:
-    st.header("Original Image")
-    st.image(img)
+if img : 
+    with col1:
+        st.header("Original Image")
+        st.image(img)
 
 if img and number_of_segments > 0:
     img = Image.open(img)
